c0fe1f8023a8584f5e3f19ad7ada436f845df8df
César Sala Gago <125811102+CesarSala92@users.noreply.github.com>
2023-09-07 12:33:45 +0200

    Add files via upload

---
+++ CS-Tools.tab/Otros.panel/Crear Subproyectos.pushbutton/script.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "Crear \nSubproyectos"
+__autor__ = "César Sala Gago"
+__doc__ = """Crea subproyectos a partir de un archivo .txt con los nombres separados por comas.
+"""
+
+
+#---------------IMPORTACIONES----------------
+
+import clr
+
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import *
+from RevitServices.Transactions import *
+
+
+
+import System
+from System.Collections.Generic import *
+
+from pyrevit import forms
+
+#------------------VARIABLES------------------
+uidoc = __revit__.ActiveUIDocument
+app = __revit__.Application
+doc = __revit__.ActiveUIDocument.Document
+
+
+#-----------------FUNCIONES-------------------
+
+listaSubproyectos = list()
+if doc.IsWorkshared:
+    archivoBase = forms.pick_file(file_ext='txt')
+
+    if archivoBase:
+        miArchivo = open(archivoBase, "r")
+        archivoLeer = miArchivo.read()
+        archivoLista = archivoLeer.split(",")
+
+        for elemento in archivoLista:
+            listaSubproyectos.append(str(elemento.replace("\n","")))
+        
+        with Transaction(doc, __title__) as t:
+            t.Start()
+            for subproyecto in listaSubproyectos:
+                nuevoSubproyecto = Workset.Create(doc, subproyecto)
+            t.Commit()
+
+    else:
+        res = forms.alert("No se ha seleccionado ningún archivo",
+                  ok=True)
+else:
+    res = forms.alert("El archivo no está compartido",
+                  ok=True)
